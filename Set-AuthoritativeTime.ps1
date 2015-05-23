f141f3719893ae4fe152e4a6817c6ce486b1fba9
Shahrad Rezaei <shahrad.rezaei@shah-network.com>
2015-05-23 09:52:35 -0400

    Added helper function to concatenate domain names.
    Added a privately scoped function, Concatenate-DomainNames
    to concatenate a list of domain names in a way that the
    w32tm will accept.

--- Set-AuthoritativeTime.ps1
+++ Set-AuthoritativeTime.ps1
@@ -1,5 +1,12 @@
 function Set-AuthoritativeTime
 {
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String[]]
+        $TimeServer
+    )
+    
     Stop-Service w32time;
     
     w32tm /config `
@@ -13,4 +20,25 @@ function Set-AuthoritativeTime
             /nowait
     
     Start-Service w32time;
+}
+
+function Private:Concatenate-DomainNames
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String[]] $DomainName
+    )
+    
+    for($i = 0; $i -lt $DomainName.Count; $i++)
+    {
+        if ($i -ne 0)
+        {
+            $DomainString += ","
+        }
+        
+        $DomainString += $DomainName[$i]
+    }
+    
+    Return $DomainString.Replace('\n', '')
 }
