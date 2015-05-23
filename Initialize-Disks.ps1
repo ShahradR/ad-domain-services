684aed1de63c28ea985b3fc341d536c4844ea357
Shahrad Rezaei <shahrad.rezaei@shah-network.com>
2015-05-23 09:16:55 -0400

    Resolved merge conflict (for real).

--- Initialize-Disks.ps1
+++ Initialize-Disks.ps1
@@ -4,17 +4,11 @@
         
     If (@($disks).Count -gt 0)
     {
-<<<<<<< HEAD
         foreach ($disk in $disks)
         {
             $disk |`
             New-Partition -AssignDriveLetter -UseMaximumSize | `
             Format-Volume -FileSystem NTFS -Confirm:$false
         }
-=======
-        $disk |`
-        New-Partition -AssignDriveLetter -UseMaximumSize | `
-        Format-Volume -FileSystem NTFS -Confirm:$false
->>>>>>> 18385b359c6d2535428b95f424ccad997ba7ad56
     }
 }
