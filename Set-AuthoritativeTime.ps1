3a49ba28df0c05e2f415c1d63cd4bb0b491e0433
Shahrad Rezaei <shahrad.rezaei@shah-network.com>
2015-05-29 22:42:09 -0400

    Removed variable parameters.
    Removed the variable parameters for the time server, since
    entering such complex parameters in SCVMM is not very
    intuitive. As such, the Concatenate-DomainNames function
    has been removed from the Set-AuthoritativeTime file, and
    the user is now expected to provide the list of time
    servers as a comma delimited list.

--- Set-AuthoritativeTime.ps1
+++ Set-AuthoritativeTime.ps1
@@ -3,13 +3,13 @@ function Set-AuthoritativeTime
     Param
     (
         [Parameter(Mandatory=$true)]
-        [String[]]
+        [String]
         $TimeServer
     )
     
     w32tm /config `
             /update `
-            /manualpeerlist:"time.nrc.ca,time.chu.nrc.ca" `
+            /manualpeerlist:$TimeServer `
             /syncfromflags:MANUAL `
             /reliable:YES
         
@@ -18,25 +18,4 @@ function Set-AuthoritativeTime
             /nowait
     
     Restart-Service w32time;
-}
-
-function Private:Concatenate-DomainNames
-{
-    Param
-    (
-        [Parameter(Mandatory=$true)]
-        [String[]] $DomainName
-    )
-    
-    for($i = 0; $i -lt $DomainName.Count; $i++)
-    {
-        if ($i -ne 0)
-        {
-            $DomainString += ","
-        }
-        
-        $DomainString += $DomainName[$i]
-    }
-    
-    Return $DomainString.Replace('\n', '')
 }
