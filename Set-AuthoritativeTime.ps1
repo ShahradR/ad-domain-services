3e5956d95d31b175f6d9864e0b1ee41ef258d01a
Shahrad Rezaei <shahrad@rezaei.io>
2019-04-20 12:31:09 -0400

    Add ShouldProcess support in Set-AuthoritativeTime
    Add ShouldProcessSupport in the Set-AuthoritativeTime function around
    the w32tm functions, based on feedback from the PowerShell Script
    Analyzer. This adds support for the WhatIf and Confirm PowerShell flags.
    
    Associated issues: GH-2

--- Set-AuthoritativeTime.ps1
+++ Set-AuthoritativeTime.ps1
@@ -1,5 +1,10 @@
 function Set-AuthoritativeTime
 {
+    [CmdletBinding(
+        SupportsShouldProcess = $true,
+        ConfirmImpact = "High"
+    )]
+
     Param
     (
         [Parameter(Mandatory=$true)]
@@ -7,15 +12,17 @@ function Set-AuthoritativeTime
         $TimeServer
     )
 
-    w32tm /config `
+    if ($PSCmdlet.ShouldProcess("$_", "Update time server configuration")) {
+        w32tm /config `
             /update `
             /manualpeerlist:$TimeServer `
             /syncfromflags:MANUAL `
             /reliable:YES
 
-    w32tm /resync `
+        w32tm /resync `
             /rediscover `
             /nowait
 
-    Restart-Service w32time;
+        Restart-Service w32time;
+    }
 }
